38031cc330c6e8e4a5328b056579af3b9e574c16
Gustavo Moraes <gustavo.moraes_external@wartsila.com>
2025-04-19 16:12:00 +0200

    Implement unit tests

--- internal/http/router.go
+++ internal/http/router.go
@@ -3,6 +3,7 @@ package http
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/guttosm/url-shortener/docs"
+	"github.com/guttosm/url-shortener/internal/auth"
 	"github.com/guttosm/url-shortener/internal/middleware"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -11,27 +12,26 @@ import (
 // NewRouter sets up the HTTP routes for the application.
 //
 // Parameters:
-// - handler (*Handler): The HTTP handler containing the logic for URL shortening and redirection.
+// - handler (*Handler): The HTTP handler containing the logic for URL shortening and login.
+// - validator (auth.TokenValidator): The service used to validate JWT tokens.
 //
 // Returns:
-// - *gin.Engine: The configured Gin router with all the necessary routes.
-//
-// Routes:
-// - POST /api/shorten: Handles URL shortening requests.
-// - GET /:shortID: Redirects to the original URL based on the shortened ID.
-// - GET /swagger/*any: Serves the Swagger API documentation.
-func NewRouter(handler *Handler) *gin.Engine {
+// - *gin.Engine: The configured Gin router with public and protected endpoints.
+func NewRouter(handler *Handler, validator auth.TokenValidator) *gin.Engine {
 	router := gin.Default()
 
-	api := router.Group("/api")
-	{
-		api.POST("/login", handler.Login)
-	}
-
+	// Swagger documentation
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
+	// Public routes
+	public := router.Group("/api")
+	{
+		public.POST("/login", handler.Login)
+	}
+
+	// Protected routes
 	protected := router.Group("/api")
-	protected.Use(middleware.AuthMiddleware())
+	protected.Use(middleware.AuthMiddleware(validator))
 	{
 		protected.POST("/shorten", handler.ShortenURL)
 	}
